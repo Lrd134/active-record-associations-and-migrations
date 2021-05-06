18c88682c32ed8296b6bcc8e02c780e70d5a77df
Lrd134 <larryc3200@gmail.com>
2021-05-06 14:34:41 -0400

    commit tree 27342066fadc92ab91e1b27d868b73a1d40f6871

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
+    def up
+        create_table :artists do |t|
+            t.string :name
+          end
+    end
+
+    def down
+        drop_table :artists
+    end
 end
--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,2 +1,10 @@
 class AddArtistToSongs < ActiveRecord::Migration[4.2]
+    # def up
+    #     add_column :songs, :artist, :foreign_key
+    #     add_column :songs, :genre, :foreign_key
+    # end
+    # def down
+    #     remove_column :songs, :artist
+    #     remove_column :songs, :genre
+    # end
 end
